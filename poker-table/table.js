405cfd538df653a11cf6ac2252c01a69f7e03c91
eio3 <Ethaniorlowsky@gmail.com>
2020-12-18 21:47:31 -0800

    make minor adjustments to gui

--- poker-table/table.js
+++ poker-table/table.js
@@ -13,22 +13,22 @@ let app = new Vue({
         player_playing: 0,
         players: [
             { name: 'rivy33', bank: 100, onTable: 77, hasCards: false },
-            { name: 'kattar', color: 'cyan', bank: 100, onTable: 20, hasCards: false },
+            { name: 'kattar', color: 'cyan', bank: 100, onTable: 20, hasCards: true },
             { name: 'mikelaire', color: 'lightcoral', bank: 100, onTable: 20, hasCards: false },
-            { name: 'tomtom', color: 'crimson', bank: 100, onTable: 20, hasCards: false },
-            { name: 'nana', color: '#444', bank: 100, onTable: 20, hasCards: false },
+            { name: 'tomtom', color: 'crimson', bank: 100, onTable: 20, hasCards: true },
+            { name: 'nana', color: '#444', bank: 100, onTable: 20, hasCards: true },
             { name: 'ionion', color: 'forestgreen', bank: 100, onTable: 20, hasCards: false },
             { name: 'link6996', color: 'goldenrod', bank: 100, onTable: 20, hasCards: false },
             { name: 'gossboganon', color: 'gold', bank: 100, onTable: 20, hasCards: false }
         ],
         figures: [
-            'P',
+            'S',
             'H',
             'C',
             'D'
         ],
         values: [
-            '1',
+            'A',
             '2',
             '3',
             '4',
@@ -38,8 +38,8 @@ let app = new Vue({
             '8',
             '9',
             '10',
-            'V',
-            'D',
+            'J',
+            'Q',
             'K'
         ]
     },
@@ -66,3 +66,37 @@ let app = new Vue({
         }
     }
 })
+
+// My js additions
+
+// turn players grey if they don't have cards
+function updatePlayerColors() {
+    for (let i = 1; i <= app.players.length; i++) {
+        const p = app.players[i - 1];
+        const icon = document.querySelector(`.player-${i}`).querySelector('.avatar');
+        if (p.hasCards) {
+            icon.style.backgroundColor = p.color || 'dodgerblue';
+        }
+        else {
+            icon.style.backgroundColor = 'grey';
+        }
+    }
+}
+
+updatePlayerColors();
+
+for (let i = 1; i <= app.players.length; i++) {
+    const icon = document.querySelector(`.player-${i}`).querySelector('.avatar');
+    icon.addEventListener('click', function () { togglePlayer(i) });
+}
+
+function refreshPage() {
+    location.reload();
+}
+
+function togglePlayer(playerNum) {
+    console.log(`You clicked on player ${playerNum}`);
+    const player = app.players[playerNum - 1];
+    player.hasCards = !player.hasCards;
+    updatePlayerColors();
+}
